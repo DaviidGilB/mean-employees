05ff747b176e8f1c8a994ddadb3993306592a10a
David Gil Baeza <davidgil.univ@gmail.com>
2019-07-02 12:27:12 +0200

    Mitad frontend aprox

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { EmployeesComponent } from './components/employees/employees.component';
+import { EmployeesComponent } from
+'./components/employees/employees.component';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { EmployeesComponent } from './components/employees/employees.component';
     EmployeesComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
